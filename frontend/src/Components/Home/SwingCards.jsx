f94aa356fb7fd853b20fcba903e483aa98186ba0
Otengtech <otengebenezer326@gmail.com>
2025-07-28 01:58:27 -0700

    commit

--- frontend/src/Components/Home/SwingCards.jsx
+++ frontend/src/Components/Home/SwingCards.jsx
@@ -4,13 +4,11 @@ import { PiGraphLight } from "react-icons/pi";
 
 const HeroImageWithCards = () => {
   return (
-    <div className="w-full md:w-1/2 mt-10 md:mt-0 flex flex-col md:ml-20 justify-center relative">
-
-      {/* Floating Cards - Centered */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex flex-col lg:flex-row items-center justify-center gap-4 px-4">
-        
+    <div className="w-full bg-gradient-to-r from-gray-900 to-violet-900 flex flex-col justify-center items-center">
+      {/* Cards Container */}
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-4 mt-10">
         {/* Card 1 */}
-        <div className="backdrop-blur-md bg-white/10 border border-white/20 p-4 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing">
+        <div className="backdrop-blur-md bg-white/10 border border-white/20 px-4 py-6 md:px-5 md:py-8 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing">
           <FaUsers className="text-sky-400 text-3xl" />
           <div className="text-white text-sm font-semibold">
             2000+ <br />
@@ -19,7 +17,7 @@ const HeroImageWithCards = () => {
         </div>
 
         {/* Card 2 */}
-        <div className="backdrop-blur-md bg-white/10 border border-white/20 p-4 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing delay-[200ms]">
+        <div className="backdrop-blur-md bg-white/10 border border-white/20 px-4 py-6 md:px-5 md:py-8 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing delay-200">
           <FaReact className="text-sky-400 text-3xl" />
           <div className="text-white text-sm font-semibold">
             Built with <br />
@@ -28,7 +26,7 @@ const HeroImageWithCards = () => {
         </div>
 
         {/* Card 3 */}
-        <div className="backdrop-blur-md bg-white/10 border border-white/20 p-4 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing delay-[400ms]">
+        <div className="backdrop-blur-md bg-white/10 border border-white/20 px-4 py-6 md:px-5 md:py-8 rounded-xl shadow-md w-60 flex items-center gap-3 animate-swing delay-400">
           <PiGraphLight className="text-sky-400 text-3xl" />
           <div className="text-white text-sm font-semibold">
             95% <br />
